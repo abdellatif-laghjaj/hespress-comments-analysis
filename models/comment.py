430a8b8f54f92c2c4cefd47f738eff4125438f1f
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-11-27 19:25:22 +0100

    Update comment.py

--- models/comment.py
+++ models/comment.py
@@ -6,10 +6,10 @@ from pydantic import BaseModel
 class Comment(BaseModel):
     user_name: str
     comment_text: str
-    date: datetime
+    date: str
     likes: int
     article_url: str
     article_title: str
     sentiment: Optional[str] = 'NEUTRAL'
     batch_id: Optional[str] = None
-    processing_timestamp: datetime = datetime.now()
+    processing_timestamp: str = datetime.now().isoformat()
