b93e00fa6742a012a0c17a70c1cbeafd1910682f
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-12-01 14:18:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import logging
+import schedule
+import time
+import traceback
 from storage.hdfs_handler import HDFSHandler
 from config.mongodb_config import MongoDBConfig
 from config.kafka_config import KafkaConfig
@@ -7,8 +10,8 @@ from storage.kafka_handler import KafkaHandler
 from processors.batch_processor import BatchProcessor
 import six
 import sys
-
 from utils.sentiments_processor import SentimentProcessor
+from datetime import datetime
 
 # Setup logging
 logging.basicConfig(
@@ -23,6 +26,16 @@ logger = logging.getLogger(__name__)
 
 if sys.version_info >= (3, 12, 0):
     sys.modules['kafka.vendor.six.moves'] = six.moves
+
+
+def scheduled_batch_process(batch_processor, urls):
+    try:
+        logger.info(f"Starting scheduled batch processing at {datetime.now()}")
+        batch_id = batch_processor.process_urls(urls)
+        logger.info(f"Finished scheduled batch processing for batch {batch_id} at {datetime.now()}")
+    except Exception as e:
+        logger.error(f"Error during scheduled batch processing: {str(e)}")
+        logger.error(traceback.format_exc())  # Include traceback for detailed error info
 
 
 def main():
@@ -47,24 +60,27 @@ def main():
 
         # Initialize processors
         batch_processor = BatchProcessor(
-            mongodb_handler,
-            kafka_handler,
-            hdfs_handler,
-            sentiment_processor
+            mongodb_handler=mongodb_handler,
+            hdfs_handler=hdfs_handler,
+            kafka_handler=kafka_handler,
+            sentiment_processor=sentiment_processor
         )
 
         # Example URLs to process
         urls = [
-            "https://www.hespress.com/%d8%b8%d9%87%d9%88%d8%b1-%d8%a8%d9%88%d8%b1%d9%8a%d8%b7%d8%a9-%d8%a5%d9%84%d9%89-%d8%ac%d8%a7%d9%86%d8%a8-%d8%b9%d8%b1%d8%a7%d9%82%d8%ac%d9%8a-%d9%8a%d8%b3%d8%a7%d8%a6%d9%84-%d8%ad%d9%82%d9%8a%d9%82-1471306.html",
+            "https://www.hespress.com/%d8%a7%d9%84%d9%85%d9%84%d9%83-%d9%8a%d8%b7%d8%a7%d9%84%d8%a8-%d8%a8%d8%a7%d9%84%d8%aa%d8%ad%d8%b1%d9%83-%d8%a7%d9%84%d8%b9%d8%a7%d8%ac%d9%84-%d9%88%d8%a7%d9%84%d9%81%d9%88%d8%b1%d9%8a-%d9%84%d9%88-1471217.html",
         ]
 
-        # Process batch
-        batch_id = batch_processor.process_urls(urls)
-        logger.info(f"Successfully processed batch {batch_id}")
+        # Schedule the batch processing job (e.g., every minute)
+        schedule.every().minute.do(scheduled_batch_process, batch_processor, urls)
+
+        # Keep the application running and check for scheduled jobs
+        while True:
+            schedule.run_pending()
+            time.sleep(60)  # Check every minute
 
     except Exception as e:
         logger.error(f"Critical error in main process: {str(e)}")
-        import traceback
         logger.error(traceback.format_exc())
 
 
